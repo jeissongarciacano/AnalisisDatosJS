f4d7355b32621c4f3cf00a1f94fb420a856cfccc
jeisson garcia <jei.isson.1999@gmail.com>
2022-04-06 09:24:30 -0500

    add more data select

--- resource.js
+++ resource.js
@@ -8,15 +8,17 @@ let ejey;
 
 function Main() {
 
-    clear();
-
     ejex = document.getElementById("ejex").value;
     ejey = document.getElementById("ejey").value;
+
+    clear();
 
     if(ejex == 0 || ejey == 0){
         window.alert("seleccione un dato");
     }
     else{
+        madedata(ejex,1);
+        madedata(ejey,2);
         d3.csv("Data.csv").then((data) => {
             showData(data);
         })
@@ -26,6 +28,71 @@ function Main() {
 //limpiar datos
 function clear(){
     body.selectAll("*").remove();
+}
+//crear el text
+function madedata(id,axis){
+    var text = "";
+    switch (id) {
+        case "Tratamiento":
+            text = "<br>Tratamiento:<br>1 = KMC menos de 2001g<br>2 = Control menos de 2001g<br>3 = Mas de 2500g ";
+            break;
+        case "Pretermino":
+            text = "<br>Prematuro:<br>0 = No es prematuro<br><br>1 = Es prematuro";
+            break;
+        case "BIRTH_cesarea":
+            text = "<br>Parto:<br>0 = Parto por cesarea<br>1 = Parto natural";
+            break;
+        case "BIRTH_sexo5":
+            text = "<br>Genero:<br>1 = Femenino<br>2 = Masculino";
+            break; 
+        case "BIRTH_peso5":
+            text = "<br>Peso: gramos<br>Valido más de 491g<br>0 = Datos perdidos";
+            break;
+        case "BIRTH_talla5":
+            text = "<br>Talla: milimetros<br>Valido más de 485mm<br>6 <= D , Datos perdidos";
+            break; 
+        case "BIRTH_pc5":
+            text = "<br>Circunferencia de la cabeza: milimetros<br>Valido más de 406mm<br>85 <= D , Datos perdidos";
+            break;
+        case "FOLL12M_peso12":
+            text = "<br>Peso: gramos<br>Valido más de 449g<br>42 <= D , Datos perdidos";
+            break;
+        case "FOLL12M_talla12":
+            text = "<br>Talla: milimetros<br>Valido más de 444mm<br>47 <= D , Datos perdidos";
+            break;
+        case "FOLL12M_pc12":
+            text = "<br>Circunferencia de la cabeza: milimetros<br>Valido más de 451mm<br>40 <= D , Datos perdidos";
+            break;
+        case "FOLL12M_infanib12":
+            text = "<br>Neuromotor test:<br>0 = No se realizo la evaluacion<br><br>1 = Normal<br><br>2 = Transitorio<br><br>3 = Anormal";
+            break;
+        case "aseg_cerveletWMtotal":
+            text = "<br>Cervelet WM total:<br>Valido más de 299<br>192 <= D , Datos perdidos";
+            break; 
+        case "aseg_cerveletGMtotal":
+            text = "<br>Cervelet GM total:<br>Valido más de 299<br>192 <= D , Datos perdidos";
+            break;
+        case "aseg_hipocampototal":
+            text = "<br>Cipocampo total:<br>Valido más de 299<br>192 <= D , Datos perdidos";
+            break; 
+        case "aseg_caudatetotal":
+            text = "<br>Caudate total:<br>Valido más de 299<br>192 <= D , Datos perdidos";
+            break;
+        case "aseg_putamentotal":
+            text = "<br>Putamen total:<br>Valido más de 299<br>192 <= D , Datos perdidos";
+            break;
+        case "aseg_amigdalatotal":
+            text = "<br>Amigdala total:<br>Valido más de 299<br>192 <= D , Datos perdidos";
+            break;
+        default:
+            break;
+    }
+    insertdata(text,axis);
+}
+//insertar datos en el dom
+function insertdata(text,axis){
+    if(axis == 1) document.getElementById("dataejex").innerHTML = text;
+    else document.getElementById("dataejey").innerHTML = text;
 }
 
 function showTooltip(text, coords) {
@@ -99,9 +166,9 @@ function showData(clients) {
             })
             .on("mousemove", (d) => {
                 let dat = "Ver mas"
-                if (d.BIRTH_sexo5 == 2) {
+                /*if (d.BIRTH_sexo5 == 2) {
                   dat = "Mujer";
-                }
+                }*/
                 showTooltip(dat, [d3.event.clientX, d3.event.clientY + 30])
             })
             .on("mouseleave", (d) => {
@@ -111,8 +178,9 @@ function showData(clients) {
                 selecteddata = d[ejex];
                 selecteddata2 = d[ejey];
                 showGraphics(clients,d);
-            }).merge(container).style("fill", d => d[ejex] == selecteddata && d[ejey] == selecteddata2 ? "red" : "orange") //change color
-
+            })
+            .merge(container)
+            .style("fill", d => d[ejex] == selecteddata && d[ejey] == selecteddata2 ? "red" : "orange") //change color
     });
     container.call(zoom)
 
